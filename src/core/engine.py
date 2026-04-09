5ab0c6b925595ac20ff0b88cdd72d03795290273
Helton Melo <heltonjonasmelo@gmail.com>
2026-04-09 19:36:34 -0300

    Update engine.py

--- src/core/engine.py
+++ src/core/engine.py
@@ -181,7 +181,8 @@ class AnalisadorADMWeb:
                 cv2.putText(img_rgb, status, (35, 53), cv2.FONT_HERSHEY_DUPLEX, 0.7, (255, 255, 255), 2)
                 cv2.putText(img_rgb, f"{int(val_ang)} deg", (d_b[0]-40, d_b[1]+45), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 255, 255), 2)
             
-            frames.append(img_rgb)
+            frame_reduzido = cv2.resize(img_rgb, (640, 480))
+            frames.append(frame_reduzido)
             angulos.append(val_ang)
             velocidades.append(vel_ang)
             if progress_callback: 
